e941ae8bb5379e3632e01ce1bce41e22ef9b0c96
Nayan kumar singh <grtnayan6@gmail.com>
2024-01-17 16:13:04 +0000

    Added conversion&Operations file.

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+// let mark = 56
+
+// let conv = String(mark)
+// console.log(mark)
+// console.log(typeof(mark))
+
+// console.log(conv)
+// console.log(typeof(conv))
+
+let mark = "56f"
+
+let conv = Number(mark)
+// console.log(mark)
+// console.log(typeof(mark))
+
+// console.log(conv)
+// console.log(typeof(conv))
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "nayan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "nayan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let apna = undefined
+
+let pp= String(apna)
+// console.log(pp)
+// console.log(typeof(pp))
+
+let value = 33
+let negv = -value
+//console.log(negv)
+
+//console.log(2**3)  //power
+//console.log(2/3)  
+// console.log(1+2+"3");
+// console.log("3"+2+9);
+
+console.log(+false);
+console.log(+true);
+
+console.log(+"");
